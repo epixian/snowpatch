27bb59581561e58a29e18fb241ff6c35212c5882
epixian <epixian@epixian.com>
2018-12-21 02:22:12 -0500

    add validation rules to Contact model

--- app/Contact.php
+++ app/Contact.php
@@ -6,10 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+	protected static $validate = [
+		'fname' => 'required',
+		'lname' => 'required',
+		'title' => 'nullable',
+		'work_phone' => 'nullable',
+		'mobile_phone' => 'nullable',
+		'email_1' => 'nullable|email',
+		'email_2' => 'nullable|email'
+	];
     protected $guarded = [];
 
 	public function organization()
 	{
 		return $this->belongsTo(Organization::class);
+	}
+
+	public static function validated()
+	{
+		return self::$validate;
 	}
 }
