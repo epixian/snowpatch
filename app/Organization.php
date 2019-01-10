7efe06d02c3ae5bad6edb16ed7346306058bdb19
epixian <epixian@epixian.com>
2019-01-10 09:37:50 -0500

    clean up code

--- app/Organization.php
+++ app/Organization.php
@@ -65,14 +65,6 @@ class Organization extends Model
 		return $this->hasMany(Jobsite::class);
 	}
 	
-	/**
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-	public function contacts()
-	{
-		return $this->hasMany(Contact::class);
-	}
-	
     /**
      * @return array|null
      */
@@ -90,19 +82,88 @@ class Organization extends Model
 	}
 	
 	/**
-	 * @param \App\Contact
-	 */
-	public function addContact($contact)
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+	public function contacts()
 	{
-		$this->contacts()->create(compact('contact'));
+		return $this->hasMany(Contact::class);
 	}
 	
+	/**
+	 * @param array The validated array of contact info
+	 * @param boolean Whether to set this contact as primary
+	 */
+	public function addContact($attributes, $setPrimaryContact = false)
+	{
+		// dd($attributes, $setPrimaryContact);
+		$contact = $this->contacts()->create($attributes);
+		if ($setPrimaryContact || $this->contacts->count() == 1) 
+		{
+			$this->primary_contact_id = $contact->id;
+		}
+		$this->save();
+	}
+
+	/**
+	 * @param array The validated array of contact info
+	 * @param boolean Whether to set this contact as primary
+	 */
+	public function updateContact($attributes, $setPrimaryContact = false)
+	{
+		// dd($attributes, $setPrimaryContact);
+		$contact = $this->contacts()->update($attributes);
+		if ($setPrimaryContact || $this->contacts->count() == 1) 
+		{
+			$this->primary_contact_id = $contact->id;
+		}
+		$this->save();
+	}
+
+	/**
+	 * @param \App\Contact The Contact to delete
+	 */
+	public function deleteContact(Contact $contact)
+	{
+		// dd($attributes, $setPrimaryContact);
+		dd($contact->belongsTo());
+		if ($contact->isPrimaryContact()) 
+		{
+			if ($this->contacts->count() == 1) {
+				$this->primary_contact_id = null;
+				$this->contacts->find($contact->id)->delete();
+			}
+			else
+			{
+				$this->contacts;
+			}
+		}
+		$contact = $this->contacts()->find($contact->id);
+
+		$this->save();
+	}
+
 	/**
 	 * @param \App\Contact
 	 */
 	public function setPrimaryContact($contact)
 	{
 		$this->primary_contact_id = $contact->id;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function hasPrimaryContact()
+	{
+		return $this->primary_contact_id != null;
+	}
+
+	/**
+	 * @return \App\Contact Retrieves primary contact as set by setPrimaryContact.  Assumes hasPrimaryContact = true.
+	 */
+	public function getPrimaryContact()
+	{
+		return $this->contacts->find($this->primary_contact_id);
 	}
 }
 
