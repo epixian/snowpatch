35db7c6ea65c315b2eb7b9e0cba8c0ef348ba072
epixian <epixian@epixian.com>
2019-01-24 01:15:16 -0500

    fixed doc comments

--- app/Http/Controllers/OrganizationsController.php
+++ app/Http/Controllers/OrganizationsController.php
@@ -54,7 +54,7 @@ class OrganizationsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Jobsite  $jobsite
+     * @param  \App\Organization  $organization
      * @return \Illuminate\Http\Response
      */
 	public function show(Organization $organization)
@@ -65,7 +65,7 @@ class OrganizationsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Jobsite  $jobsite
+     * @param  \App\Organization  $organization
      * @return \Illuminate\Http\Response
      */
 	public function edit(Organization $organization)
@@ -76,8 +76,7 @@ class OrganizationsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Jobsite  $jobsite
+     * @param  \App\Organization  $organization
      * @return \Illuminate\Http\Response
      */
 	public function update(Organization $organization)
@@ -90,7 +89,7 @@ class OrganizationsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Jobsite  $jobsite
+     * @param  \App\Organization  $organization
      * @return \Illuminate\Http\Response
      */
 	public function destroy(Organization $organization)
