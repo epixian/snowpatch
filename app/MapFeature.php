e5ca858202ccaa57eac3379295e5d32890d3beb9
epixian <epixian@epixian.com>
2019-02-19 11:36:24 -0500

    new functions for retrieval and storage of GeoJSON collections: asGeoJSON and fromGeoJSON

--- app/MapFeature.php
+++ app/MapFeature.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class MapFeature extends Model
@@ -52,4 +53,77 @@ class MapFeature extends Model
     	return $this->belongsTo(Map::class);
     }
 
+    public static function asGeoJSON($map, $description = 'all')
+    {
+        $result = DB::table('map_features')
+            ->selectRaw('ST_AsGeoJSON(geometry) as geometry, description')
+            ->where('map_id', $map->id)
+            ->get();
+
+        // build GeoJSON object
+        $features = [];
+        for ($i = 0; $i < count($result); $i++) 
+        {
+            if($result[$i]->description == $description || $description == 'all')
+            {
+                $feature = [
+                    'type' => 'Feature',
+                    'geometry' => json_decode($result[$i]->geometry),
+                    'properties' => [
+                        'description' => $result[$i]->description
+                    ]
+                ];
+                array_push($features, $feature);
+            }
+        }
+        $json = ['type' => 'FeatureCollection', 'features' => $features];
+        return json_encode($json);
+    }
+
+    public static function fromGeoJson($map, $json, $updateCenter = true)
+    {
+        $json = json_decode($json);
+
+        // map each description/activity to its stored-as geometry type
+        $descriptions = [
+            'Plowing' => 'Polygon',
+            'Shoveling' => 'LineString',
+            'Entry' => 'Point'
+        ];
+
+        DB::beginTransaction();
+
+        // delete previous features
+        DB::table('map_features')
+            ->where('map_id', $map->id)
+            ->delete();
+
+        // insert new/updated features
+        foreach ($json->features as $feature)
+        {
+
+            DB::table('map_features')
+                ->insert([
+                    'map_id' => $map->id,
+                    'geometry' => DB::raw('ST_GeomFromGeoJSON(\'' . json_encode($feature->geometry) . '\')'),
+                    'description' => '\'' . $feature->properties->description . '\''
+                ]);
+        }
+
+        // set new map center
+        $result = DB::table('map_features')
+            ->selectRaw('ST_AsGeoJSON(ST_Envelope(ST_GeomFromGeoJSON(\'' . json_encode($json) . '\'))) as bounds')
+            ->limit(1)
+            ->get();
+        $mbr = json_decode($result[0]->bounds)->coordinates[0];
+        if ($updateCenter) 
+        {
+            $map->setCenter([
+                'lat' => ($mbr[2][1] + $mbr[0][1]) / 2,
+                'lng' => ($mbr[2][0] + $mbr[0][0]) / 2
+            ]);
+        }
+
+        DB::commit();
+    }
 }
